6dc1288f3fa890818326ec2694f3428234e6d5a0
iamdayanand <dayananddhumala@gmail.com>
2026-09-03 15:08:15 +0200

    Update main.tf

--- lessons/day03/main.tf
+++ lessons/day03/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Create a S3 bucket
 resource "aws_s3_bucket" "tf_test_baivab_bucket" {
-  bucket = "my-tf-test-baiv-bucket-10111nandu"
+  bucket = "my-tf-test-baiv-bucket-10111nandu101"
 
   tags = {
     Name        = "My bucket"
@@ -22,13 +22,4 @@ resource "aws_s3_bucket" "tf_test_baivab_bucket" {
   }
 }
 
-# Create a S3 bucket
-resource "aws_s3_bucket" "tf_test_baivab_bucket2" {
-  bucket = "my-tf-test-baiv-bucket-10111dayanand"
-
-  tags = {
-    Name        = "My bucket2"
-    Environment = "Test"
-  }
-}
 
